4e1790195bcdad044e198e7dc3a1d20ffc5c5e0d
Kukjun Lee <lxx3380@gmail.com>
2022-09-29 22:42:24 +0900

    <feature> : <NoLogin api 필요 목록 가져오기>
    SimpleTest Component 생성.
    그리고 API를 가져오기 위한 data 설정
    
    Resolves: #issueNo
    See also: #issueNo

---
+++ src/components/SimpleTest.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card>
+    <v-img :src="symbol_image_root" :aspect-ratio="16 / 9" :width="250">
+    </v-img>
+    <v-card-text>
+      <v-row>
+        <v-col cols="12"> {{ maker_nickname }} / {{ company }}</v-col>
+        <v-col cols="12" class="text--primary"> title : {{ title }} </v-col>
+        <v-col cols="12"> 지급 Point : {{ reward }}p </v-col>
+        <v-col cols="12">
+          신청/모집 인원 : {{ apply }}/{{ participant_capacity }}
+        </v-col>
+        <v-col cols="4"></v-col>
+        <v-col cols="4"></v-col>
+        <v-col cols="4" class="grey lighten-1 text-center">
+          D-{{ deadline_remain }}</v-col
+        >
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'SimpleTest',
+  data() {
+    return {
+      id: null,
+      title: '',
+      maker_nickname: '',
+      company: '',
+      deadline_remain: null,
+      reward: null,
+      apply: null,
+      participant_capacity: null,
+      symbol_image_root: '',
+    };
+  },
+  methods: {
+    insertMockValue() {
+      this.id = 1;
+      this.title = '제목입니다';
+      this.maker_nickname = 'Maker국준';
+      this.company = 'Kkuk Company';
+      this.deadline_remain = 10;
+      this.reward = 5000;
+      this.apply = 15;
+      this.participant_capacity = 100;
+      this.symbol_image_root =
+        'https://cdn.vuetifyjs.com/images/parallax/material.jpg';
+    },
+  },
+  created() {
+    this.insertMockValue();
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,8 +7,6 @@
             >Tester Matching Platform에 오신것을 환영합니다.</span
           >
         </v-col>
-      </v-row>
-      <v-row>
         <v-col cols="8">
           <span
             >Tester Matching Platform에서 한번에 여러 테스트 공모에 참여하고,
@@ -18,43 +16,29 @@
         <v-col cols="4">
           <v-btn @click="routeRegisterToTester"> Tester로 시작하기</v-btn>
         </v-col>
-      </v-row>
-      <v-row class="pa-3">
         <v-col cols="6">
-          <v-card>
+          <v-card class="pa-5">
             <v-row>
               <v-col cols="12">
                 <div class="text-h5 pa-5">마감임박 Test</div>
               </v-col>
-            </v-row>
-            <v-row>
-              <v-col v-for="n in 4" :key="n" cols="6"
-                ><v-sheet
-                  color="white"
-                  elevation="1"
-                  height="200"
-                  width="200"
-                ></v-sheet
-              ></v-col>
+              <v-col v-for="n in 4" :key="n" cols="6">
+                <SimpleTest></SimpleTest>
+              </v-col>
             </v-row>
           </v-card>
         </v-col>
         <v-col cols="6">
-          <v-card>
+          <v-card class="pa-5">
             <v-row>
               <v-col cols="12">
                 <div class="text-h5 pa-5">인기있는 Test</div>
               </v-col>
             </v-row>
             <v-row>
-              <v-col v-for="n in 4" :key="n" cols="6"
-                ><v-sheet
-                  color="white"
-                  elevation="1"
-                  height="200"
-                  width="200"
-                ></v-sheet
-              ></v-col>
+              <v-col v-for="n in 4" :key="n" cols="6">
+                <SimpleTest></SimpleTest>
+              </v-col>
             </v-row>
           </v-card>
         </v-col>
@@ -64,10 +48,8 @@
     <div class="pa-10">
       <v-row>
         <v-col cols="12">
-          <span class="text-h3">혹시 Test를 제작하고자 하시나요?.</span>
+          <span class="text-h3">혹시 Test를 제작하고자 하시나요?</span>
         </v-col>
-      </v-row>
-      <v-row>
         <v-col cols="8">
           <span
             >간편하고 효율적으로 Tester들을 모으고 기능들을 시험해보세요
@@ -81,62 +63,30 @@
         <v-row>
           <v-col cols="6">
             <v-card>
-              <v-row>
-                <v-col cols="12">
-                  <span class="text-h5 pa-5">등록된 Tester의 수</span>
-                </v-col>
-              </v-row>
-              <v-sheet
-                color="white"
-                elevation="1"
-                height="150"
-                width="500"
-              ></v-sheet>
+              <div class="text-h5 pa-5">등록된 Tester의 수</div>
+              <div class="text-h3 text-center ma-5">{{ testerCount }}명</div>
             </v-card>
           </v-col>
           <v-col cols="6">
             <v-card>
-              <v-row>
-                <v-col cols="12">
-                  <span class="text-h5 pa-5">등록된 Maker의 수</span>
-                </v-col>
-              </v-row>
-              <v-sheet
-                color="white"
-                elevation="1"
-                height="150"
-                width="500"
-              ></v-sheet>
+              <div class="text-h5 pa-5">등록된 Maker의 수</div>
+              <div class="text-h3 text-center ma-5">{{ makerCount }}명</div>
             </v-card>
           </v-col>
           <v-col cols="6">
             <v-card>
-              <v-row>
-                <v-col cols="12">
-                  <span class="text-h5 pa-5">신청중인 Test 개수</span>
-                </v-col>
-              </v-row>
-              <v-sheet
-                color="white"
-                elevation="1"
-                height="150"
-                width="500"
-              ></v-sheet>
+              <div class="text-h5 pa-5">신청중인 Test 개수</div>
+              <div class="text-h3 text-center ma-5">
+                {{ continueTestCount }}개
+              </div>
             </v-card>
           </v-col>
           <v-col cols="6">
             <v-card>
-              <v-row>
-                <v-col cols="12">
-                  <span class="text-h5 pa-5">완료한 Test 개수</span>
-                </v-col>
-              </v-row>
-              <v-sheet
-                color="white"
-                elevation="1"
-                height="150"
-                width="500"
-              ></v-sheet>
+              <div class="text-h5 pa-5">완료한 Test 개수</div>
+              <div class="text-h3 text-center ma-5">
+                {{ completeTestCount }}개
+              </div>
             </v-card>
           </v-col>
         </v-row>
@@ -146,9 +96,18 @@
 </template>
 
 <script>
+import SimpleTest from '@/components/SimpleTest';
 export default {
   name: 'MainView',
-
+  components: { SimpleTest },
+  data() {
+    return {
+      testerCount: 0,
+      makerCount: 0,
+      continueTestCount: 0,
+      completeTestCount: 0,
+    };
+  },
   methods: {
     routeRegisterToTester() {
       this.$router.push('/register');
@@ -156,6 +115,15 @@ export default {
     routeRegisterToMaker() {
       this.$router.push('/register');
     },
+    initMockValue() {
+      this.testerCount = 100;
+      this.makerCount = 50;
+      this.continueTestCount = 20;
+      this.completeTestCount = 5;
+    },
+  },
+  created() {
+    this.initMockValue();
   },
 };
 </script>
